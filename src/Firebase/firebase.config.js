0abd2f3842522a33d763ef22e1673f679d2ddb78
NazmulIslam95 <nazmulislamctg@gmail.com>
2023-10-05 10:12:00 +0600

    my first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBT3EYOqR6rkyeoVHyqHkcetbv5gbzIrNY",
+    authDomain: "dragon-news-paper-2fe92.firebaseapp.com",
+    projectId: "dragon-news-paper-2fe92",
+    storageBucket: "dragon-news-paper-2fe92.appspot.com",
+    messagingSenderId: "634129218246",
+    appId: "1:634129218246:web:2ba6a99727db0344a5f5c7"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
